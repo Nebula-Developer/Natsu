d7b2ae1844e6e1a4bcabb20ebde8e5720de8bb00
NebulaDev <nebuladev.contact@gmail.com>
2025-02-05 23:47:08 +1300

    Make `Begin` and `After` append the sequence to the transformable after creation

--- Natsu/Extensions/TransformableExtensions.cs
+++ Natsu/Extensions/TransformableExtensions.cs
@@ -1,15 +1,26 @@
-using Natsu.Core;
 using Natsu.Mathematics.Transforms;
 
 namespace Natsu.Extensions;
 
 public static class TransformableExtensions {
-    public static TransformSequence<T> AppendToTransformable<T>(this TransformSequence<T> sequence, Element elm) where T : ITransformable {
+    public static TransformSequence<T> AppendToTransformable<T>(this TransformSequence<T> sequence, T elm) where T : ITransformable {
         elm.AddTransformSequence(sequence);
         return sequence;
     }
 
-    public static TransformSequence<T> Begin<T>(this T transformable, string name) where T : ITransformable => new(transformable) { Name = name };
+    /// <summary>
+    ///     Starts a new <see cref="TransformSequence{T}" /> with the given name.
+    /// </summary>
+    /// <param name="transformable">The <see cref="ITransformable" /> to start the sequence on</param>
+    /// <param name="name">The name of the sequence</param>
+    /// <returns>The newly created <see cref="TransformSequence{T}" /></returns>
+    public static TransformSequence<T> Begin<T>(this T transformable, string name) where T : ITransformable => new TransformSequence<T>(transformable) { Name = name }.AppendToTransformable(transformable);
 
-    public static TransformSequence<T> After<T>(this T transformable, float delay = 0) where T : ITransformable => new TransformSequence<T>(transformable).Then(delay);
+    /// <summary>
+    ///     Starts a new <see cref="TransformSequence{T}" /> with the given delay.
+    /// </summary>
+    /// <param name="transformable">The <see cref="ITransformable" /> to start the sequence on</param>
+    /// <param name="delay">The delay before the sequence starts</param>
+    /// <returns>The newly created <see cref="TransformSequence{T}" /></returns>
+    public static TransformSequence<T> After<T>(this T transformable, float delay = 0, string name = "") where T : ITransformable => new TransformSequence<T>(transformable) { Name = name }.Then(delay).AppendToTransformable(transformable);
 }
