c8281fb205b4e2a747a5f6f13067a1ab7ae9f7cf
NebulaDev <nebuladev.contact@gmail.com>
2025-02-17 21:35:47 +1300

    Create and publish `NativeLibs` project, add package reference in core project

--- Natsu.Sandbox/Program.cs
+++ Natsu.Sandbox/Program.cs
@@ -1,7 +1,10 @@
-using Natsu.Platforms.Desktop;
+using Natsu.Platforms.Audio.BassPlatform;
 
 namespace Natsu.Sandbox;
 
 public static class Program {
-    public static void Main(string[] args) => new DesktopWindow(new MyApp()).Run();
+    public static void Main(string[] args) {
+        BassAudio.Init();
+        // new DesktopWindow(new MyApp()).Run();
+    }
 }
---
+++ Natsu/Platforms/Audio/BassPlatform/BassAudio.cs
@@ -0,0 +1,11 @@
+
+
+using ManagedBass;
+
+namespace Natsu.Platforms.Audio.BassPlatform;
+
+public static class BassAudio {
+    public static void Init() {
+        Bass.Init();
+    }
+}
